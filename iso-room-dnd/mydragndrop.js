4711924aca637be97c7a6940e8de3357a5656bfd
LeoB-Dev <h0undgr21n@proton.me>
2025-10-31 20:42:35 +0000

    Added collapsable sidebar to dragndrop page

--- iso-room-dnd/mydragndrop.js
+++ iso-room-dnd/mydragndrop.js
@@ -84,12 +84,22 @@ slots.forEach((slot) => {
 
 
 function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
+    const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
+
+    const mainNavItems = document.querySelectorAll('.hideOnMobile');
+    for (const navItem of mainNavItems) {
+        navItem.style.display = 'none';
+    }
 }
 
 function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
+    const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
+    
+    const mainNavItems = document.querySelectorAll('.hideOnMobile');
+    for (const navItem of mainNavItems) {
+        navItem.style.display = 'flex';
+    }
 }
 
