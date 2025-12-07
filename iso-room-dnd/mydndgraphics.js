1b1d5c73c5deee054bdd394937233f3536b33b37
LeoB-Dev <173692627+LeoB-Dev@users.noreply.github.com>
2025-12-07 14:14:55 +0000

    draggable items can now be appended onto room in good positions

--- iso-room-dnd/mydndgraphics.js
+++ iso-room-dnd/mydndgraphics.js
@@ -4,7 +4,7 @@ $.get("../navbar.html", function(data){
 
 
 // CodePen drag and drop version (need to change room elements to id's not classes), thus I am temporarily only selecting this class https://codepen.io/BitsPls/pen/XWvwVpE
-const dropZones = document.getElementsByClassName("room-bottom");
+const dropZones = document.getElementsByClassName("dropzone");
 
 for (const zone of dropZones) {
     zone.addEventListener("dragover", (e) => {
@@ -26,8 +26,26 @@ for (const zone of dropZones) {
         const draggedElement = document.querySelector(".dragging");
         if (draggedElement) {
             zone.appendChild(draggedElement);
-            console.log('child element appended to dropzone');
-            draggedElement.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(-210deg);");  // 1 / 0.864, you need to divide by 0.864 which is the same as multipling by 1/0.864
+
+            if (zone.id === "room-bottom") {
+                console.log('child element appended to bottom dropzone');
+                draggedElement.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(-210deg);");  // 1 / 0.864, you need to divide by 0.864 which is the same as multipling by 1 / 0.864
+                draggedElement.style.left = "20%";
+                draggedElement.style.top = "20%";
+            }
+            else if (zone.id === "room-left") {
+                console.log('child element appended to left dropzone');
+                draggedElement.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(30deg);");
+                draggedElement.style.left = "35%";
+                draggedElement.style.top = "10%";
+
+            } 
+            else if (zone.id === "room-right") {
+                console.log('child element appended to right dropzone');
+                draggedElement.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(-90deg);");
+
+            }
+            
         }
     });
 }
