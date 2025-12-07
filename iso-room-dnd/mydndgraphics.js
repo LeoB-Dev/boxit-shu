53bb633fbc004aa30ec4d888dd33a604c183e309
LeoB-Dev <173692627+LeoB-Dev@users.noreply.github.com>
2025-12-07 13:35:21 +0000

    draggable items can now be appended to room bottom

--- iso-room-dnd/mydndgraphics.js
+++ iso-room-dnd/mydndgraphics.js
@@ -2,35 +2,84 @@ $.get("../navbar.html", function(data){
     $("#nav").replaceWith(data);
 });
 
-let newX = 0, newY = 0, startX = 0, startY = 0;
 
-const furniture = document.getElementsByClassName('furniture')[1];
+// CodePen drag and drop version (need to change room elements to id's not classes), thus I am temporarily only selecting this class https://codepen.io/BitsPls/pen/XWvwVpE
+const dropZones = document.getElementsByClassName("room-bottom");
 
-furniture.addEventListener('mousedown', mouseDown)
+for (const zone of dropZones) {
+    zone.addEventListener("dragover", (e) => {
+        e.preventDefault(); // Parentheses invoke function - browsers default to not allowing elements to be drop targets
+        zone.classList.add("drag-over");
+        console.log('dragged over');
+    });
 
-function mouseDown(e){
-    startX = e.clientX
-    startY = e.clientY
+    zone.addEventListener("dragleave", () => {
+        zone.classList.remove("drag-over");
+        console.log('drag left');
+    });
 
-    document.addEventListener('mousemove', mouseMove)
-    document.addEventListener('mouseup', mouseUp)
+    zone.addEventListener("drop", (e) => {
+        e.preventDefault();
+        zone.classList.remove("drag-over");
+        console.log('dropped');
+
+        const draggedElement = document.querySelector(".dragging");
+        if (draggedElement) {
+            zone.appendChild(draggedElement);
+            console.log('child element appended to dropzone');
+            draggedElement.setAttribute("style", "transform: scaleY(1.157) skew(30deg) rotate(-210deg);");  // 1 / 0.864, you need to divide by 0.864 which is the same as multipling by 1/0.864
+        }
+    });
 }
 
-function mouseMove(e){
-    newX = startX - e.clientX 
-    newY = startY - e.clientY 
+document.body.addEventListener("dragstart", (e) => {
+    if (e.target.classList.contains("furniture")) {
+        e.target.classList.add("dragging");
+        console.log("dragging started");
+    }
+});
+
+document.body.addEventListener("dragend", (e) => {
+    if (e.target.classList.contains("furniture")) {
+        e.target.classList.remove("dragging");
+        console.log("dragging stopped");
+    }
+});
+
+
+
+
+
+// Drag and drop to anywhere on the screen original code:
+
+// let newX = 0, newY = 0, startX = 0, startY = 0;
+
+// const furniture = document.getElementsByClassName('furniture')[1];
+
+// furniture.addEventListener('mousedown', mouseDown)
+
+// function mouseDown(e){
+//     startX = e.clientX
+//     startY = e.clientY
+
+//     document.addEventListener('mousemove', mouseMove)
+//     document.addEventListener('mouseup', mouseUp)
+// }
+
+// function mouseMove(e){
+//     newX = startX - e.clientX 
+//     newY = startY - e.clientY 
   
-    startX = e.clientX
-    startY = e.clientY
+//     startX = e.clientX
+//     startY = e.clientY
 
-    furniture.style.top = (furniture.offsetTop - newY) + 'px'
-    furniture.style.left = (furniture.offsetLeft - newX) + 'px'
-}
+//     furniture.style.top = (furniture.offsetTop - newY) + 'px'
+//     furniture.style.left = (furniture.offsetLeft - newX) + 'px'
+// }
 
-function mouseUp(e){
-    document.removeEventListener('mousemove', mouseMove)
-}
-
+// function mouseUp(e){
+//     document.removeEventListener('mousemove', mouseMove)
+// }
 
 
 function showSidebar(){
