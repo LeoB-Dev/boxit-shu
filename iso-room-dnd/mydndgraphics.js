0edb58b78f0d56d7aeb948cfb7833646698c1111
LeoB-Dev <173692627+LeoB-Dev@users.noreply.github.com>
2025-12-07 15:01:13 +0000

    draggable items can now be appended onto room in good positions

--- iso-room-dnd/mydndgraphics.js
+++ iso-room-dnd/mydndgraphics.js
@@ -66,40 +66,6 @@ document.body.addEventListener("dragend", (e) => {
 
 
 
-
-
-// Drag and drop to anywhere on the screen original code:
-
-// let newX = 0, newY = 0, startX = 0, startY = 0;
-
-// const furniture = document.getElementsByClassName('furniture')[1];
-
-// furniture.addEventListener('mousedown', mouseDown)
-
-// function mouseDown(e){
-//     startX = e.clientX
-//     startY = e.clientY
-
-//     document.addEventListener('mousemove', mouseMove)
-//     document.addEventListener('mouseup', mouseUp)
-// }
-
-// function mouseMove(e){
-//     newX = startX - e.clientX 
-//     newY = startY - e.clientY 
-  
-//     startX = e.clientX
-//     startY = e.clientY
-
-//     furniture.style.top = (furniture.offsetTop - newY) + 'px'
-//     furniture.style.left = (furniture.offsetLeft - newX) + 'px'
-// }
-
-// function mouseUp(e){
-//     document.removeEventListener('mousemove', mouseMove)
-// }
-
-
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
