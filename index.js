42c8c396e56cf1af452da6a40496f7196a850330
Leo B Dev <173692627+LeoB-Dev@users.noreply.github.com>
2025-12-15 14:58:27 +0000

    made sidebar openable on local host

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 $.get("navbar.html", function(data){
     $("#nav").replaceWith(data);
 });
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+
+    const mainNavItems = document.querySelectorAll('.hideOnMobile');
+    for (const navItem of mainNavItems) {
+        navItem.style.display = 'none';
+    }
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+    
+    const mainNavItems = document.querySelectorAll('.hideOnMobile');
+    for (const navItem of mainNavItems) {
+        navItem.style.display = 'flex';
+    }
+}
